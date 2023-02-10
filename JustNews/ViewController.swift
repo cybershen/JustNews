8b0434855384d32a239414bb555d4b94913bfc10
cybershen <cyberpuer@proton.me>
2023-02-10 16:17:55 +0200

    Initial Commit

---
+++ JustNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustNews
+//
+//  Created by Назар Жиленко on 10.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
