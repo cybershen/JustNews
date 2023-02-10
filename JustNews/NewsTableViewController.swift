e000c5c666fc34dbfe1fdedd7dcfb53a4aad8e03
cybershen <cyberpuer@proton.me>
2023-02-10 16:40:04 +0200

    Setting Up the User Interface

--- JustNews/NewsTableViewController.swift
+++ JustNews/NewsTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  NewsTableViewController.swift
 //  JustNews
 //
 //  Created by Назар Жиленко on 10.02.2023.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
 }
-
